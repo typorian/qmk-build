5fe503c2f9aa6bb26be0461ae197af45ecbdbeda
Romāns Potašovs <justgook@gmail.com>
2023-05-11 16:15:34 +0300

    debuging: fixing firmware build (attemp 3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,9 @@ echo "qmk compile -kb $KEYBOARD -km $KEYMAP"
 echo "----------------------------------------"
 ls
 echo "----------------------------------------"
+echo "----------------------------------------"
+ls /qmk_firmware/keyboards/idobao/id75/keymaps/
+echo "----------------------------------------"
 ln -s "$KEYMAP_PATH" "/qmk_firmware/$KEYMAP_PATH"
 qmk compile -kb "$KEYBOARD" -km "$KEYMAP"
 rm -rf build
