df8bd2139914c5e5a514eef13dc4df65def13760
typorian <eidorian@posteo.de>
2024-12-15 20:24:02 +0100

    Update keymap.c

--- keyboards/preonic/keymaps/addy/keymap.c
+++ keyboards/preonic/keymaps/addy/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
-  KC_LCTL, KC_LGUI, KC_LALT, LOWER, RAISE,   LT(1,KC_SPC),  LT(1,KC_SPC),  KC_ENT,   KC_LEFT, KC_UP, KC_DOWN,   KC_RGHT  \
+  KC_LCTL, KC_LGUI, KC_LALT, LOWER, RAISE,   LT(1,KC_SPC),  LT(1,KC_SPC),  RALT_T(KC_ENT),   KC_LEFT, KC_UP, KC_DOWN,   KC_RGHT  \
 ),
 
 [_SPACEFN] = LAYOUT_preonic_grid( \
@@ -156,7 +156,7 @@ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UUMLAUT, KC_NO, OUMLAUT, KC_NO, KC_NO,  \
-  KC_NO,  AUMLAUT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  KC_NO,  AUMLAUT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_EQL, KC_LBRC,  KC_RBRC, KC_MINUS, KC_BSLS, \
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, _______, \
   _______, _______, _______, LOWER, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
@@ -175,8 +175,8 @@ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid( \
-  RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, KC_NO, KC_NO,  \
-  _______, RESET,   _______,   VRSN,    _______, _______, _______, _______, _______,_______, _______, KC_NO,  \
+  RGB_TOG, UG_PREV, UG_NEXT, UG_VALD, UG_VALU, UG_HUED, UG_HUEU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  \
+  _______, RESET,   QK_BOOT,   VRSN,    _______, _______, _______, _______, _______,_______, _______, KC_NO,  \
   _______, _______, MU_NEXT,  AU_ON,   AU_OFF,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,  _______, _______, \
   _______, AU_NEXT,  AU_PREV,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD  \
