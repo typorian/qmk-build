7d073eacdb75485724c935f278b2ff1913f6c4b9
typorian <eidorian@posteo.de>
2024-12-16 21:47:16 +0100

    more adjustments split and layers

--- keyboards/preonic/keymaps/addy/keymap.c
+++ keyboards/preonic/keymaps/addy/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SPACEFN] = LAYOUT_preonic_grid( \
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, \
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(7), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, \
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(8), KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, \
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, \
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
 ),
@@ -177,7 +177,7 @@ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 [_ADJUST] = LAYOUT_preonic_grid( \
   RGB_TOG, UG_PREV, UG_NEXT, UG_VALD, UG_VALU, UG_HUED, UG_HUEU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  \
   _______, RESET,   QK_BOOT,   VRSN,    _______, _______, _______, _______, _______,_______, _______, KC_NO,  \
-  _______, _______, MU_NEXT,  AU_ON,   AU_OFF,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,  _______, _______, \
+  _______, _______, MU_NEXT,  AU_ON,   AU_OFF,  TO(8), KC_NO, KC_NO,  KC_NO, KC_NO,  _______, _______, \
   _______, AU_NEXT,  AU_PREV,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD  \
 ),
@@ -193,8 +193,8 @@ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 /*9*/
 
 [_GAMING] = LAYOUT_preonic_grid( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_HOME,  \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TO(0),  \
   KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO,   KC_SPC,  KC_SPC,  KC_ENT,   KC_LEFT, KC_UP, KC_DOWN,   KC_RGHT  \
